25ff1e6b58bd24f9071e58a9d9f7b05bce6ca8d1
Comiskey <jcomiskey@nps.gov>
2022-02-14 10:14:18 -0500

    a

--- VersionControl.R
+++ VersionControl.R
@@ -3,7 +3,7 @@ setwd("~/D/R_Training/Day4")
 # This opens up a browser window where you can create a personal access token. It may ask you to log into GitHub or confirm your password.
 usethis::create_github_token()
 gitcreds::gitcreds_set()
-
+ 
 # git@github.com:jcomiskey/git-practice.git
 
 # Test change 3  
